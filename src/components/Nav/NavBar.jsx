cf17c59d4dd05ec01e570518edf31bb1b81fb2b3
Abdul Alsafri <abdulalsafri1@gmail.com>
2024-08-02 12:28:49 -0500

    NavBar now has a menue icon that displays a sidebar menu when clicked.

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,35 +1,86 @@
 import { Link, useNavigate } from "react-router-dom";
-import "./NavBar.css";
+import "/src/index.css";
+import { useState } from "react";
 
 export const NavBar = ({ currentUser }) => {
   const navigate = useNavigate();
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!isSidebarVisible);
+  };
+
   return (
-    <ul className="navbar">
-      <li className="navbar-item">
-        <Link to="/destinations">Destinations</Link>
-      </li>
-      <li className="navbar-item">
-        <Link to="/explorations">Explorations</Link>
-      </li>
-      <li className="navbar-item">
-        <Link to="/journey">My Journey</Link>
-      </li>
-      {localStorage.getItem("trip-ify_user") ? (
-        <li className="navbar-item navbar-logout">
-          <Link
-            className="navbar-link"
-            to=""
-            onClick={() => {
-              localStorage.removeItem("trip-ify_user");
-              navigate("/", { replace: true });
-            }}
-          >
-            Logout
-          </Link>
+    <>
+      <div className="navbar">
+        <ul className="nav-links">
+          <li>
+            <Link to="/destinations">Destinations</Link>
+          </li>
+          <li>
+            <Link to="/explorations">Explorations</Link>
+          </li>
+          <li>
+            <Link to="/journey">My Journey</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          {localStorage.getItem("trip-ify_user") && (
+            <li>
+              <Link
+                to=""
+                onClick={() => {
+                  localStorage.removeItem("trip-ify_user");
+                  navigate("/", { replace: true });
+                }}
+              >
+                Logout
+              </Link>
+            </li>
+          )}
+        </ul>
+        <div className="menu-button" onClick={toggleSidebar}>
+          <a href="#">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#5f6368"
+            >
+              <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+            </svg>
+          </a>
+        </div>
+      </div>
+      <ul className={`sidebar ${isSidebarVisible ? "visible" : "hidden"}`}>
+        <li>
+          <Link to="/destinations">Destinations</Link>
         </li>
-      ) : (
-        ""
-      )}
-    </ul>
+        <li>
+          <Link to="/explorations">Explorations</Link>
+        </li>
+        <li>
+          <Link to="/journey">My Journey</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        {localStorage.getItem("trip-ify_user") && (
+          <li>
+            <Link
+              to=""
+              onClick={() => {
+                localStorage.removeItem("trip-ify_user");
+                navigate("/", { replace: true });
+              }}
+            >
+              Logout
+            </Link>
+          </li>
+        )}
+      </ul>
+    </>
   );
 };
