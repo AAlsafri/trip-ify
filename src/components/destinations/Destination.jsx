4317d8cc1e020b1fcaf8e2441fd1f36379c2e5d5
Abdul Alsafri <abdulalsafri1@gmail.com>
2024-08-08 21:13:48 -0500

    Added styles for favorites.

--- src/components/destinations/Destination.jsx
+++ src/components/destinations/Destination.jsx
@@ -7,24 +7,31 @@ export const Destination = ({ destination, onDelete }) => {
 
   return (
     <section className="destination">
-      <header>
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <Link to={`/destinations/${destination.id}`}>
           <h3>
             #{destination.id} {destination.name}
           </h3>
         </Link>
+        {destination.isLiked && (
+          <span className="heart-emoji" role="img" aria-label="heart">
+            ❤️
+          </span>
+        )}
       </header>
       <div>{destination.details}</div>
       <footer>
-        <div>
-          <div>Is this a favorite destination?</div>
-          <div>{destination.isLiked ? "Yes!" : "No."}</div>
-        </div>
-        <button className="button" onClick={handleDelete}>
+        <button onClick={handleDelete} className="button">
           Delete
         </button>
         <Link to={`/destinations/${destination.id}/edit`}>
-          {/* <button className="button">Edit</button> */}
+          {/* <button>Edit</button> */}
         </Link>
       </footer>
     </section>
